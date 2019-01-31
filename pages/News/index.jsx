3d61ed94c91f95513741591c4c3ca55c5d4b2ef7
Bennett Lin <bennettl@zillowgroup.com>
2019-01-30 19:55:06 -0800

    Add placeholder pages.

---
+++ pages/News/index.jsx
@@ -0,0 +1,7 @@
+import { Fragment as ___ } from 'react'
+
+export default () => (
+    <___>
+        <div>News</div>
+    </___>
+)
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,29 @@
+import { Fragment as ___ } from 'react'
+import Link from 'next/link'
+
 export default () => (
-    <p1>Bobtail Yearlings website!</p1>
+    <___>
+        <div>Bobtail Yearlings</div>
+        <div>
+            <Link
+                {...{ href: '/News' }}
+            >
+                <a>News</a>
+            </Link>
+        </div>
+        <div>
+            <Link
+                {...{ href: '/About' }}
+            >
+                <a>About</a>
+            </Link>
+        </div>
+        <div>
+            <Link
+                {...{ href: '/Lyrics' }}
+            >
+                <a>Lyrics</a>
+            </Link>
+        </div>
+    </___>
 )
